505abca9243f3d0202d4a3fb0e35a78571dc676d
KevinSimbana04 <168014823+KevinSimbana04@users.noreply.github.com>
2025-04-24 01:06:59 -0500

    Create Main_Jugador.java

---
+++ Tarea_S2/Ejercicio1/Main_Jugador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main_Jugador {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        //Atributos quemados
+
+        //objeto (Jugador-01)
+        jugador jugador1 = new jugador("Kevin Simbaña", "Defensa", 21, 15);
+
+        //objeto(Jugador-02)
+        jugador jugador2 = new jugador("Moices Caicedo","Medio Campista",23, 25);
+
+
+        //Atributo con scanner
+        System.out.print("Ingrese un Nombre");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingrese La Posición");
+        String posicion = scanner.nextLine();
+
+        System.out.print("Ingrese la edad:");
+        int edad = scanner.nextInt();
+
+        System.out.print("Ingrese el Num_Camiseta");
+        int num_camiseta = scanner.nextInt();
+
+        // objeto (Jugador-03)
+        jugador jugador3=new jugador(nombre, posicion, edad, num_camiseta);
+
+        // visualizar los atributos de cada objeto
+
+        jugador1.mostrar_informacion();
+        jugador2.mostrar_informacion();
+        jugador3.mostrar_informacion();
+
+        scanner.close();
+    }
+}
